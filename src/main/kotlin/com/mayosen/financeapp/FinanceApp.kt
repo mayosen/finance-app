5aff7d8e7391d4fc06b2c9f4c29addca7cc5982e
mayosen <gare577@gmail.com>
2025-03-18 23:37:18 +0300

    Add maven

---
+++ src/main/kotlin/com/mayosen/financeapp/FinanceApp.kt
@@ -0,0 +1,11 @@
+package com.mayosen.financeapp
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FinanceappDuplicateApplication
+
+fun main(args: Array<String>) {
+	runApplication<FinanceappDuplicateApplication>(*args)
+}
