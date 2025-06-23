2acdd8588201963a22cadbd4af1c4b67aa44f0ab
mayosen <gare577@gmail.com>
2025-06-24 00:14:28 +0300

    Write dates as date time strings

--- command-service/src/main/kotlin/com/mayosen/financeapp/config/BeanDefinitions.kt
+++ command-service/src/main/kotlin/com/mayosen/financeapp/config/BeanDefinitions.kt
@@ -1,6 +1,8 @@
 package com.mayosen.financeapp.config
 
+import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import org.springframework.context.annotation.Bean
@@ -12,4 +14,7 @@ class BeanDefinitions {
     fun objectMapper(): ObjectMapper =
         jacksonObjectMapper()
             .registerModule(JavaTimeModule())
+            .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+            .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+            .configure(DeserializationFeature.FAIL_ON_NULL_FOR_PRIMITIVES, true)
 }
