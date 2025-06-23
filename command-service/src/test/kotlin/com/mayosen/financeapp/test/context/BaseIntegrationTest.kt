ef99dd2ffa8554229d98d9a6b5cc9ddac85a4033
mayosen <gare577@gmail.com>
2025-06-24 00:28:57 +0300

    Clean up after test

--- command-service/src/test/kotlin/com/mayosen/financeapp/test/context/BaseIntegrationTest.kt
+++ command-service/src/test/kotlin/com/mayosen/financeapp/test/context/BaseIntegrationTest.kt
@@ -2,10 +2,13 @@ package com.mayosen.financeapp.test.context
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.mayosen.financeapp.event.Event
+import com.mayosen.financeapp.event.jdbc.EventEntity
+import com.mayosen.financeapp.snapshot.jdbc.AccountSnapshotEntity
 import com.mayosen.financeapp.test.context.initializer.KafkaTestContainerInitializer
 import com.mayosen.financeapp.test.context.initializer.PostgresqlTestContainerInitializer
 import com.mayosen.financeapp.test.context.kafka.KafkaTestConsumer
 import com.mayosen.financeapp.test.context.kafka.KafkaTestConsumerConfiguration
+import org.junit.jupiter.api.AfterEach
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
@@ -30,6 +33,12 @@ class BaseIntegrationTest {
 
     @Autowired
     protected lateinit var eventsCustomer: KafkaTestConsumer<Event>
+
+    @AfterEach
+    fun cleanUp() {
+        jdbcTemplate.deleteAll(EventEntity::class.java)
+        jdbcTemplate.deleteAll(AccountSnapshotEntity::class.java)
+    }
 
     protected fun stringBodyOf(body: Any): String = objectMapper.writeValueAsString(body)
 }
--- query-service/src/test/kotlin/com/mayosen/financeapp/test/context/BaseIntegrationTest.kt
+++ query-service/src/test/kotlin/com/mayosen/financeapp/test/context/BaseIntegrationTest.kt
@@ -1,12 +1,16 @@
 package com.mayosen.financeapp.test.context
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.mayosen.financeapp.projection.account.jdbc.AccountSummaryEntity
+import com.mayosen.financeapp.projection.transaction.jdbc.TransactionEntity
 import com.mayosen.financeapp.test.context.initializer.KafkaTestContainerInitializer
 import com.mayosen.financeapp.test.context.initializer.PostgresqlTestContainerInitializer
 import com.mayosen.financeapp.test.context.kafka.KafkaTestProducerConfiguration
+import org.junit.jupiter.api.AfterEach
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.core.env.Environment
 import org.springframework.data.jdbc.core.JdbcAggregateTemplate
 import org.springframework.kafka.core.KafkaTemplate
 import org.springframework.test.context.ActiveProfiles
@@ -29,6 +33,15 @@ class BaseIntegrationTest {
 
     @Autowired
     protected lateinit var eventKafkaTemplate: KafkaTemplate<String, String>
+
+    @Autowired
+    protected lateinit var environment: Environment
+
+    @AfterEach
+    fun cleanUp() {
+        jdbcTemplate.deleteAll(AccountSummaryEntity::class.java)
+        jdbcTemplate.deleteAll(TransactionEntity::class.java)
+    }
 
     protected fun stringBodyOf(body: Any): String = objectMapper.writeValueAsString(body)
 }
